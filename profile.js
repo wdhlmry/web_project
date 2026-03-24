a3dcb0f37e07c937002b80d82b6f22a0df43f181
Amira-as2203792 <as2203792@qu.edu.qa>
2026-03-24 17:12:17 +0300

    .

--- profile.js
+++ profile.js
@@ -16,6 +16,8 @@ if(pic){
 }
 
 
+
+//Handling edit form submission
 if(document.getElementsByClassName("edit_form")){
 
     document.getElementsByClassName("username").value = currentUser.username;
@@ -57,13 +59,105 @@ if(document.getElementsByClassName("edit_form")){
 
     });
 
-
-
-
-
-
-
-
-
-
 }
+
+
+
+//functions
+function updateFollowButton(){
+
+    if(currentUser.following.includes(viewedUserId)){
+        followbtn.textContent = "Following";
+    }
+    else{
+        followbtn.textContent = "Follow";
+    }
+}
+
+
+function followUser(targetUserId){
+
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+
+    const targetUser = users.find(u => u.id === targetUserId);
+
+    if(!currentUser.following.includes(targetUserId)){
+        currentUser.following.push(targetUserId)
+        targetUser.followers.push(currentUser.id);
+    }
+
+    users = users.map(user => {
+        if(user.id === currentUser.id) return currentUser;
+        if(user.id === targetUserId) return targetUser;
+        return user;
+    });
+
+    localStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("currentUser", JSON.stringify(currentUser));
+}
+
+
+function unfollowUser(targetUserId){
+
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+
+    const targetUser = users.find(u => u.id === targetUserId);
+
+    if(!currentUser.following.includes(targetUserId)){
+        alert("You are not following this user!");
+        return;
+    }
+
+    currentUser.following = currentUser.following.filter(id => id !==targetUserId);
+    targetUser.followers = targetUser.followers.filter(id => id !==currentUser.id);
+
+    users = users.map(user => {
+        if(user.id === currentUser.id) return currentUser;
+        if(user.id === targetUserId) return targetUser;
+        return user;
+    });
+
+    localStorage.setItem("users", JSON.stringify(users));
+    localStorage.setItem("currentUser", JSON.stringify(currentUser));
+}
+
+
+function updateStatus(){
+
+    document.getElementById("followers_count").textContent = currentUser.followers.length;
+    document.getElementById("following_count").textContent = currentUser.following.length;
+
+    const posts = JSON.parse(localStorage.getItem("posts")) || [];
+    const userPosts = posts.filter(post => post.userId === currentUser.id);
+
+    document.getElementById("posts_count").textContent = userPosts.length;
+}
+
+
+
+
+//Handling follow/unfollow
+const followbtn = document.getElementById("follow_button");
+
+const viewedUserId = 2; //this value is an example
+
+let users = JSON.parse(localStorage.getItem("users")) || [];
+
+let viewedUser = users.find(u => u.id === viewedUserId);
+
+
+followbtn.addEventListener("click", ()=>{
+     
+    if(currentUser.following.includes(viewedUserId)){
+        unfollowUser(viewedUserId);
+    }
+    else{
+        followUser(viewedUserId);
+    }
+    
+    updateFollowButton();
+    updateStatus();
+
+});
+
+updateFollowButton();
