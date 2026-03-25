6ffe16a06eaf9c75f78ff47a5f847c9f65eeee19
wdhlmry <wa2209529@qu.edu.qa>
2026-03-25 21:40:19 +0300

    logout.js done

---
+++ javascript/logout.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+    logoutUser();
+});
+
+function logoutUser() {
+    localStorage.removeItem("currentUser");
+}
+
+function goLogin() {
+    window.location.href = "../html/login.html";
+}
